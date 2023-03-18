272d3cc034776965ab882ea7371464852e9d8182
KIRASB <soufianebenhida00@gmail.com>
2023-03-18 06:55:21 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-		int main(void)
+int main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 		{
 	char str[] = "\"Programming is like building a multilingual puzzle\n";
-		puts (str);
+		puts(str);
 		return (0);
 }
