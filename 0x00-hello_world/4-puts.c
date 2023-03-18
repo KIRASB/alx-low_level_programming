0b73dbe5ce34053b04c4e3582d21a0ce09d0f122
KIRASB <soufianebenhida00@gmail.com>
2023-03-18 08:00:43 -0700

    hello,puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main(void)
-{
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-		puts ("\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 		}
