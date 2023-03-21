5828361a52b5b824479fb664facb44ff2ebf7aa8
KIRASB <soufianebenhida00@gmail.com>
2023-03-20 19:01:53 -0700

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	char al;
+
+	for (al = 'a' ; al <= 'z' ; al++)
+	{
+	putchar(al);
+	}
+	char CH;
+
+	for (CH = 'A' ; CH <= 'Z' ; CH++)
+	{
+		putchar(CH);
+	}
+	putchar('\n');
+	return (0);
+	}
