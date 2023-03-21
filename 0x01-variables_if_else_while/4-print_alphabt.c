6e178f2201eb71b579ef6c015a25c7c8d1e28586
KIRASB <soufianebenhida00@gmail.com>
2023-03-20 19:32:58 -0700

    druken alphabt ohne e und q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * followed by a new line
+ * Return: Aways 0 (Succes)
+ */
+int main(void)
+{
+	char la;
+
+	while (ls  <= 'z')
+	{
+
+	if (la != 'e' || la != 'q')
+	putchar(la);
+}
+	la++;
+	putchar('\n');
+	return (0);
+	}
