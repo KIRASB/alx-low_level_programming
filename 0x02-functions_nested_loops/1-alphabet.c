76c2507add2716d023ac0bc6bc8878c80ac21f35
KIRASB <soufianebenhida00@gmail.com>
2023-03-23 06:21:11 -0700

    alphabt kower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
 
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 *
-	 */
 void print_alphabet(void)
 {
 	char c;
