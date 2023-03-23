ab752ad4cc4ea10a4589e27bce201239c95783e9
KIRASB <soufianebenhida00@gmail.com>
2023-03-23 06:43:05 -0700

    alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - drukt alphabeten tzehn mal
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	}
+}
