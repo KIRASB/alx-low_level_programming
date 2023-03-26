15b2d2198312f7c0a1a5b77d913c2c5e3f8910dc
KIRASB <soufianebenhida00@gmail.com>
2023-03-25 21:52:51 -0700

    signe

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,20 +10,20 @@
 #include"main.h"
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar(48);
-                return (0);
-        }
-        else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 	return (0);
 }
