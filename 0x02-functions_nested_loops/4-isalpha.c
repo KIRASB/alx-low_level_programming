3d0f36b008b5d4089f860ca9ec32587f981a5377
KIRASB <soufianebenhida00@gmail.com>
2023-03-25 18:58:38 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,17 +6,5 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'z' && c <= 'z')
-	{
-		return (1);
-	}
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
