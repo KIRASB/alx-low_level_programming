570220cc978644ef6ddc4013ce0569a42f4f48df
KIRASB <soufianebenhida00@gmail.com>
2023-03-25 21:10:24 -0700

    signe

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,7 +17,7 @@ int print_sign(int n)
         }
         else if (n == 0)
         {
-                _putchar('0');
+                _putchar(48);
                 return (0);
         }
         else if (n < 0)
@@ -25,5 +25,4 @@ int print_sign(int n)
                 _putchar('-');
                 return (-1);
         }
-        _putchar('\n');
 }
