90db760f7bcd1fda5b71fc2667c2b7dff1c40ab1
KIRASB <soufianebenhida00@gmail.com>
2023-03-25 18:51:04 -0700

    is lower

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,21 +5,25 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
+ *
  */
+#include"main.h"
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-			return (1);
-	} else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-	return (-1);
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        else if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+        else if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+        _putchar('\n');
 }
-
