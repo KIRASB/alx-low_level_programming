dd04d0916989cad171d0cab791f465f68c7a30b0
KIRASB <soufianebenhida00@gmail.com>
2023-03-26 11:00:05 -0700

    first debugging task

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+readme if u want 
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+[>77;30603;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000
+#endif /* MAIN_H */
