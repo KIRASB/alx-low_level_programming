fcd669aa86512cee21b61f66897df675b4a1c43f
KIRASB <soufianebenhida00@gmail.com>
2023-03-25 19:39:01 -0700

    the absulute value of the int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - gives the absulute number of a number
+ * @t: is the integer of the function
+ *  Return: the absolute num of the number
+ */
+int _abs(int t)
+{
+	if  (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
