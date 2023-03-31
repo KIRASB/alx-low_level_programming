3767ac69aad83a8c2d3d0d29395beb32fa860f22
KIRASB <soufianebenhida00@gmail.com>
2023-03-30 19:49:04 -0700

    like comment and commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	/**while (i < 1)
-	{
-	putchar(i);
-	}*/
+	/*while (i < 1)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
