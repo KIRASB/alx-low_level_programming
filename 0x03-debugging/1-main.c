4e3eace88102eb274ee0047ca4939bd6a81626da
KIRASB <soufianebenhida00@gmail.com>
2023-03-31 19:03:06 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,8 +10,8 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
-	/*i = 0;
+	/*
+	 * i = 0;
 
 	while (i < 10)
 	{
@@ -19,6 +19,5 @@ int main(void)
 	}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
