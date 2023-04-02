6decba0e15de8d442e86f6e29ac6ecd28889d818
KIRASB <soufianebenhida00@gmail.com>
2023-04-02 10:55:18 -0700

    makinng a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_triangle - print a triangle
+ * @size of the tria
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	for (a = 1; a <= size; a++)
+	{
+		for (b = a; b < size; b++)
+		{
+			_putchar(' ');
+		}
+		for (b = 1; b <= a; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
