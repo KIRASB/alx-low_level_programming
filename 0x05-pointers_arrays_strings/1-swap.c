2721c97543ea8450b4bd753fa31186e5adda3fb6
KIRASB <soufianebenhida00@gmail.com>
2023-04-05 09:55:47 -0700

    sawping a and b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - it swaps the values of b and a
+ * @b: the first value or second
+ * @a: other value
+ * Return: a and b
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m
+}
