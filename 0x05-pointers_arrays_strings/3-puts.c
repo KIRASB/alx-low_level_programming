35bec76fdbe840ea0e3c04b5eeabb1f7e6a717c0
KIRASB <soufianebenhida00@gmail.com>
2023-04-11 05:40:03 -0700

    the 4th fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,11 @@
 #include "main.h"
-
+/**
+ * _puts - this function write the whole string just c by c
+ * @str: string
+ */
 void _puts(char *str)
 {
-	while ( *str != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
