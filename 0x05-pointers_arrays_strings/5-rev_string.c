0fb4d538ee3a4b10ef7476c3501da86990c4b9d1
KIRASB <soufianebenhida00@gmail.com>
2023-04-12 10:57:00 -0700

    the revers output of shar

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int a = 0;
+	int b;
+
+	while (*s != '\0')
+	{
+		a++;
+		s++;
+	}
+	s--;
+	for  (b = 0; b < a; b++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
