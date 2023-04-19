1441813ca407031419fd6a624d920f047d4f59b4
KIRASB <soufianebenhida00@gmail.com>
2023-04-19 11:23:33 -0700

    the n9z bla matrj3 lsstr function ;)

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,15 +2,22 @@
 
 void puts2(char *str)
 {
-	int a = 0;
-	int b = str[a];
-	while (b != '\0')
+	int c = 0;
+	int d;
+	char *p = str;
+
+	while (*p != '\0')
 	{
-		if (b % 2 == 0)
-		{
-			_putchar(str[a]);
-			a++;
-		}
-		a++;
+		p++;
+		c++;
 	}
+	for (d = 0; d < c; d++)
+	{
+
+		if (d % 2 == 0)
+		{
+			_putchar(str[d]);
+		}
+	}
+	_putchar('\n');
 }
