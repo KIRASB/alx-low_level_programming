d351f0152ac0620a2ad12730cf01f8249464672e
KIRASB <soufianebenhida00@gmail.com>
2023-04-21 11:22:43 -0700

    yo

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print what v been stored in the arrays
+ * @a: intput array
+ * @n: the number stored in the array a
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b != (n - 1); b++)
+	{
+		printf("%d, ", a[b]);
+	}
+	if (b == (n - 1))
+	{
+		printf("%d", a[b]);
+	}
+	printf("\n");
+}
