be5fa90321be4b89b67f75ee50fa824fb4e4b5a9
KIRASB <soufianebenhida00@gmail.com>
2023-04-25 15:56:20 -0700

    the power function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _pow_recursion - calculat the power function of two intgers
+ * @x: the base
+ * @y: the power
+ * Return: the result
  */
 int _pow_recursion(int x, int y)
 {
 	int z;
+
 	if (y == 0)
 	{
 		return (1);
@@ -13,6 +18,6 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-		z = x * _pow_recursion(x ,y - 1);
+		z = x * _pow_recursion(x, y - 1);
 		return (z);
 }
