99f61d9c8e4cc8c7086e13da91196e5a21dc3d6d
KIRASB <soufianebenhida00@gmail.com>
2023-04-25 13:22:17 -0700

    the 3rd one bro

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _strlen_recursion - give the long of a string
+ * @s: is the input string
+ * Returen:return is the long
+ */
 int _strlen_recursion(char *s)
 {
 	int a = 0;
 
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		a++;
 		a = a + _strlen_recursion(s + 1);
