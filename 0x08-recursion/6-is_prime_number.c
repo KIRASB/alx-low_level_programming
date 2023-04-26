7f8399e8bc80738e1819cb3271ddbd4c9624ad56
KIRASB <soufianebenhida00@gmail.com>
2023-04-26 07:54:23 -0700

    the prime number aaaaah math again

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,23 +11,23 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return(the_real_function(n, n - 1));
+	return (the_real_function(n, n - 1));
 }
 /**
  * the_real_function - find out if the number prime or not
  * @n: the input number
- * @i: number for  help
+ * @a: number for  help
  * Return: 1 if it's prime and 0 if it's  not
  */
 int the_real_function(int n, int a)
 {
 	if (a == 1)
 	{
-		return(1);
+		return (1);
 	}
 	if (n % a == 0 && a > 0)
 	{
-		return(0);
+		return (0);
 	}
-	return(the_real_function(n, a - 1));
+	return (the_real_function(n, a - 1));
 }
