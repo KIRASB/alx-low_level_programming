3e399b3da7d48998f94f746ad8a2a6d6b175a9ba
KIRASB <soufianebenhida00@gmail.com>
2023-06-12 18:45:32 -0700

    I fixed it

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * main - add numbers
  * @argc: number of argumments
@@ -24,7 +23,7 @@ int main(int argc, char *argv[])
 		{
 			if (*argv[a] >= '0' && *argv[a] <= '9')
 			{
-				x = atoi(argv[a]);
+				x = _atoi(argv[a]);
 				n = x;
 				z = z + n;
 			}
@@ -37,4 +36,47 @@ int main(int argc, char *argv[])
 		printf("%d\n", z);
 	}
 	return (0);
+}
+/**
+ * _atoi - it make sur that the input in int
+ * @s: the string number
+ * Return: n the intiger after fixing
+ */
+int _atoi(char *s)
+{
+	int b = 0;
+	int c = 0;
+	int d = 1;
+	int n;
+
+	if (*s == '-')
+	{
+		d = -1;
+		s++;
+	}
+	if (*s >= '0' && *s <= '9')
+	{
+		int counter = 0;
+
+		while (*s != '\0')
+		{
+			counter++;
+			s++;
+		}
+		s = s - counter;
+		while (*s != '\0')
+		{
+			for (b = 0; b <= 9; b++)
+			{
+				if (b == ((int)(*s)) - '0')
+				{
+					break;
+				}
+			}
+			c = c * 10 + b;
+			s++;
+		}
+	}
+	n = c * d;
+	return (n);
 }
