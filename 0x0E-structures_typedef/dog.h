0c5607f2f7ccbb998cf8f8209490b3b6b845ee52
KIRASB <soufianebenhida00@gmail.com>
2023-06-20 18:31:25 -0700

    mr Poppy dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the informations of a dog
+ * @name: pointer to the dog's name
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
