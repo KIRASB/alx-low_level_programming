14067477cd4b1eb2a2f8bef12d66105cdd8b4e66
KIRASB <soufianebenhida00@gmail.com>
2023-06-20 19:56:58 -0700

    the dawwg

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,11 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		d->name = "nil";
+		d->name = "(nil)";
 	}
 	if (d->owner == NULL)
 	{
-		d->owner = "nil";
+		d->owner = "(nil)";
 	}
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner)
 }
