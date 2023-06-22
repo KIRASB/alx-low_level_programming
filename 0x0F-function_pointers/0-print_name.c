9ce304c71fc8034d9a1cdec7c77b6cff47451c66
KIRASB <soufianebenhida00@gmail.com>
2023-06-21 18:14:42 -0700

    say my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - call a function to print a name
+ * @name: the parameter name
+ * @f: the pointer to the function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if(name == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}
