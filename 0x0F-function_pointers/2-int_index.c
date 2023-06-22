fcf6d92f0d2e1baf0828f17d34ae4b27865c933f
KIRASB <soufianebenhida00@gmail.com>
2023-06-21 19:46:51 -0700

    that was hard

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,19 +9,18 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
-	else
+	for (i = 0; i < size; i++)
 	{
-		int i;
-
-		for (i = 0; i > size; i++)
+		if (cmp(array[i]))
 		{
-			cmp(array[i]);
-			return (i);
+		return (i);
 		}
-		return (-1);
 	}
+	return (-1);
 }
