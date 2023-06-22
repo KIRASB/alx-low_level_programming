7de3743557a1ae0a95f2b5d927d7b050b4903a3c
KIRASB <soufianebenhida00@gmail.com>
2023-06-21 18:48:17 -0700

    the header

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - run the action function on each element of the array
+ * @array: array of intigers
+ * @size: the size of the arrays
+ * @action: the pointer to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array_iterator == NULL || action == NULL)
+	{
+		return;
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+
+		}
+	}
+}
