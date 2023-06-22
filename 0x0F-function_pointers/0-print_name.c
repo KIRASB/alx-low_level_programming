152423b9acce54bb825a673e97fe9a739cb47556
KIRASB <soufianebenhida00@gmail.com>
2023-06-21 18:17:22 -0700

    say my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * print_name - call a function to print a name
@@ -7,7 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if(name == NULL)
+	if (name == NULL)
 	{
 		return;
 	}
