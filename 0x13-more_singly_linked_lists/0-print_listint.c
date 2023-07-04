5397d5ac8ee3073547cfba91b7145bac94d70ff4
ProSalma <as.ibrahimsalma@gmail.com>
2023-07-04 06:07:15 -0700

    the more linked list tasks

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements that linked list contain
+ * @h: linked list typ of listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
