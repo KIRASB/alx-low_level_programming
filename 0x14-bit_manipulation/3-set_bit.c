8538efe640bfc4c5b94f510ab32dc7436e6cdc14
KIRASB <soufianebenhida00#gmail.com>
2023-08-31 19:40:24 -0700

    4th file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,16 +6,11 @@
  * @index: index
  * Return : returns 1 if it's work or -1 otherwise
  */
-
-int set_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
-	{
+	if (index > 63)
 		return (-1);
-	}
-	else
-	{
-		*n = ((1UL << index) | *n);
-		return(1);
-	}
+
+	*n = ((1UL << index) | *n);
+	return (1);
 }
