7826bc6accce11f6895df04ea239cfa48a497fb1
KIRASB <soufianebenhida00#gmail.com>
2023-09-04 19:46:38 -0700

    the project file io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,31 +11,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	else
-	{
-		while (textcontent != NULL)
-		{
-			c++;
-		}
-		a = open(filename, O_WRONLY | O_APEND);
-		b = write(a, text_content, c);
 
-		if (b == 0 || a == 0)
-		{
-			return (-1);
-		}
-		else
-		{
-			close(a);
-			return (1);
-		}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
-}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
